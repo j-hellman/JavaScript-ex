56fb2e0fa40ae58b60a1b039bf95020d526fb14f
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-12 16:00:41 -0300

    loop FOR, WHILE, DO WHILE

---
+++ loop-while-doWhile.js
@@ -0,0 +1,19 @@
+//Exercicio para a estrutura de repeticao WHILE
+
+
+let cont = 1
+let cont2 = 1
+
+
+//WHILE
+while (cont <= 10) {
+  console.log(`Passo ${cont}`)
+  cont++
+}
+
+
+//DO WHILE
+do {
+  console.log(`Etapa ${cont2}`)
+  cont2++
+} while (cont2 <= 10)
