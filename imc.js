7392ab0df6d8373e86ea9bc78b548cb1686d09cf
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-12 15:46:57 -0300

    calculo do imc

---
+++ imc.js
@@ -0,0 +1,37 @@
+
+// let altura = document.getElementById('txtAlt')
+// let peso = document.getElementById('txtPes')
+// let imc = document.getElementById('imc')
+
+// function calcular(){
+//   imc = Number(peso.value) / Number((altura.value ** 2))
+//   imc.innerText = `O seu IMC é de: ${imc}.`
+// }
+
+let altura = 1.82;
+let peso = 64;
+let imc = 40//(peso / (altura ** 2)).toFixed(2)
+
+console.log(`Seu IMC é de: ${imc}`)
+if (imc < 17) {
+  console.log('Extremo cuidado! Muito abaixo do peso')
+
+} else if (imc < 18.5) {
+  console.log('Cuidado! Abaixo do peso')
+
+} else if (imc < 25) {
+  console.log('Parabéns! Peso ideal.')
+
+} else if (imc < 30) {
+  console.log('Cuidado! Sobrepeso.')
+
+} else if (imc < 35) {
+  console.log('Cuidado! Obesidade.')
+
+} else if (imc < 40) {
+  console.log('Extremo cuidado! Obesidade Severa.')
+
+} else {
+  console.log('Extremo cuidado! Obesidade Mórbida.')
+}
+
