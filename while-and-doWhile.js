fc35826db70561bcda489e3b0b86875fdd32379f
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-11 23:51:07 -0300

    file update

--- while-and-doWhile.js
+++ while-and-doWhile.js
@@ -1,9 +1,19 @@
 //Exercicio para a estrutura de repeticao WHILE
 
 
-let cont = 10
+let cont = 1
+let cont2 = 1
 
-while (cont > 0) {
-  console.log('Tudo bem?')
-  cont--
+
+//WHILE
+while (cont <= 10) {
+  console.log(`Passo ${cont}`)
+  cont++
 }
+
+
+//DO WHILE
+do {
+  console.log(`Etapa ${cont2}`)
+  cont2++
+} while (cont2 <= 10)
