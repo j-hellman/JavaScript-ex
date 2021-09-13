a376f55bf2c5b6065c6f9780a4f9f80905249a92
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-13 00:12:29 -0300

    multa

---
+++ multa/multa.js
@@ -0,0 +1,25 @@
+let txtVelMax = document.getElementById('txtVelMax')
+let txtVel = document.getElementById('txtVel')
+let res = document.querySelector('p.res')
+
+function calcular() {
+  let velMax = Number(txtVelMax.value)
+  let vel = Number(txtVel.value)
+
+  if (velMax !== 0 && vel !== 0) { //Checar se os campos estao preenchidos
+    if (vel > velMax) {
+      res.innerHTML = `A velocidade maxima é de ${velMax}km/h. `
+      res.innerHTML += `Sua velocidade é de ${vel}km/h. <span>Você foi multado!</span>`
+
+    } else
+      res.innerHTML = ' Dirija com segurança. Use o cinto.'
+
+  } else
+    res.innerHTML = 'Favor preencher os campos.'
+}
+
+function limpar() { //Funcao para limpar campos apos consulta realizada
+  txtVelMax.value = ''
+  txtVel.value = ''
+  res.innerText = ''
+}
