879d21a0aa731a667b7d23491ee9e34c2f552ad4
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-13 00:12:04 -0300

    contagem-inteligente

---
+++ contagem-inteligente.js
@@ -0,0 +1,24 @@
+// Exercicio: contagem regressiva ou progressiva de acordo com o numero inicial e final
+
+let inicio = 0;
+let fim = 8;
+
+console.log('CONTAGEM INTELIGENTE')
+console.log('--------------------')
+console.log(`Início: ${inicio}`)
+console.log(`Fim: ${fim}`)
+console.log('--------------------')
+console.log('CONTANDO')
+console.log('--------------------')
+
+if (fim > inicio) {
+  for (let i = inicio; i <= fim; i++) {
+    console.log(`${i}... `)
+  }
+  
+} else {
+  for (let i = inicio; i >= fim; i--) {
+    console.log(`${i}... `)
+  }
+}
+
