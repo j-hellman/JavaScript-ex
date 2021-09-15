52a00d0694e8d76449e58bb3d637fbef5947ec8a
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-15 11:21:09 -0300

    file update

--- fatorial.js
+++ fatorial.js
@@ -1,15 +1,15 @@
 //Exercicio para achar o fatorial de um numero
 // 5! = 5x4x3x2x1
 
-function fatorial(n) {
-  let fat = 1
-  for (let c = n; c > 1; c--) {
-    fat *= c
-  }
-  return fat
-}
+// function fatorial(n) {
+//   let fat = 1
+//   for (let c = n; c > 1; c--) {
+//     fat *= c
+//   }
+//   return fat
+// }
 
-console.log(fatorial(5))
+// console.log(fatorial(5))
 
 
 //Exemplo de maneira recursiva (funcao chamando funcao)
