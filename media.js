9dc7f82c7765fae6241259f17230057f8f0b2103
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-15 11:22:45 -0300

    media

---
+++ media.js
@@ -0,0 +1,25 @@
+/*
+let nota1 = 8;
+let nota2 = 7;
+let media = 7 //(Number(nota1) + Number(nota2)) / 2
+
+console.log('--------------------')
+console.log('MÉDIA FINAL DO ALUNO')
+console.log('--------------------')
+
+console.log(`Primeira nota: ${nota1}`)
+console.log(`Segunda nota: ${nota2}`)
+
+console.log('--------------------')
+console.log(`Média: ${media}`)
+
+// Operador Ternario
+media < 7 ? console.log('Aluno REPROVADO') : console.log('Aluno APROVADO')
+console.log('--------------------')
+
+*/
+let lim = 25
+
+for (let i = 1; i <= lim; i++) {
+console.log(i)
+}
