ed1f3a28e5481464d59f849ca8196b6dab8546c2
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-15 20:20:02 -0300

    voto

---
+++ voto.js
@@ -0,0 +1,15 @@
+// Exercicio: idade votacao
+
+let idade = 15
+
+if (idade < 16) {
+  console.log('Não vota')
+
+} else if (idade < 18 || idade > 65) {
+  console.log('Voto facultativo')
+
+} else {
+  console.log('Voto obrigatório')
+
+}
+
