6eecd124d27fef8c0cbe3876524e29a75af695da
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-16 10:35:23 -0300

    js code final

--- idade/idade.js
+++ idade/idade.js
@@ -1,26 +1,29 @@
   let data = new Date() //Data do sistema operacional
   let anoAtual = data.getFullYear()
 
-  let anoNasc = document.getElementById('anoNasc') //Input do ano nascimento
-  let masc = document.getElementById('masc') //Input masculino
-  let fem = document.getElementById('fem') //Input feminino
-  let img = document.getElementById('imagem')
+  //Inputs
+  let anoNasc = document.getElementById('anoNasc')
+  let masc = document.getElementById('masc') 
+  let fem = document.getElementById('fem') 
+  let img = document.querySelector('.imagem') 
+  //
 
-  let sexo = '' //Var para receber o sexo
-
-  let msg = document.getElementById('msg') //Mensagem
+  let sexo = '' //Variavel para receber o sexo informado
+  let msg = document.querySelector('.msg') //Mensagem
 
   // Botao verificar
   function verificar() {
-    if (anoNasc.value !== '') { //Condicao para campo nascimento vazio
-      if (masc.checked || fem.checked) { //Condicao para campo sexo vazio
+    if (anoNasc.value !== '') { //Verifica campo nascimento vazio
+      if (masc.checked || fem.checked) { //Verifica campo sexo vazio
         
         let idade = anoAtual - Number(anoNasc.value) //Calculo da idade
         
-        if (masc.checked) { //Checa se homem
+        img.style.display = 'block' //Volta a mostrar a imagem
+        msg.style.textAlign = 'center' //Alinha msg ao centro
+        
+        //Se homem
+        if (masc.checked) {
           sexo = 'Homem'
-          img.style.display = 'block' //Volta a mostrar a imagem
-          msg.style.textAlign = 'center' //Alinha ao centro a mensagem
 
           if (idade < 18) {
             img.src = "./assets/h-crianca.jpeg"
@@ -30,16 +33,25 @@
 
           } else img.src = "./assets/h-idoso.jpeg"
         }
+        //
 
+        //Se mulher
+        if (fem.checked) {
+          sexo = 'Mulher'
+        
+          if (idade < 18) {
+            img.src = "./assets/m-crianca.jpeg"
 
+          } else if (idade < 65) {
+            img.src = "./assets/m-adulta.jpeg"
 
-        // if (fem.checked) sexo = 'Mulher'
+          } else img.src = "./assets/m-idosa.jpeg"
+        }
+        //
     
+        //Mensagem
         msg.innerText = `Detectamos ${sexo} com ${idade} anos`
 
-
-
       } else alert('Favor informar o sexo.')
-
     } else alert('Favor informar o ano de nascimento.')
   }
