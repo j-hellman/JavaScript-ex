a4f6540d6d7454da8e6ee3220eb60c1f324a1417
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-18 01:17:09 -0300

    js code

--- contagem-intervalo/contagem.js
+++ contagem-intervalo/contagem.js
@@ -4,5 +4,19 @@ function contar() {
   let fim = document.getElementById('txtfim')
   let passo = document.getElementById('txtpasso')
   let res = document.getElementById('res')
+  let total = []
 
+  let emojis = [
+    128073, //mao 
+    127937 //bandeira
+  ]
+
+  for(let soma = Number(inicio.value); soma <= fim.value; soma += Number(passo.value)) {
+    total.push(soma)
+    total.push(String.fromCodePoint(emojis[0])) //Emoji mao
+  }
+  
+  total.push(String.fromCodePoint(emojis[1])) //Emoji bandeira
+  res.innerHTML = 'Contando: <br />'
+  res.innerHTML += total
 }
