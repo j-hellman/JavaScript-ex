f36cdc2c5ce602668b070c3a1ef27a2592498c35
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-20 01:02:43 -0300

    cleaning code

--- loop-for.js
+++ loop-for.js
@@ -5,16 +5,13 @@
 // i++ => increment expression
 
 // Option1: normal
-for (let i = 0; i < 5; i++) {
-  console.log('Hello World');
-}
+for (let i = 0; i < 5; i++)
+  console.log('Hello World')
 
 // Option2: inverse
-for (let i = 5; i > 0; i--) {
-  console.log('Hello World');
-}
+for (let i = 5; i > 0; i--)
+  console.log('Hello World')
 
 // Option 3: odd numbers
-for (let i = 0; i < 5; i++) {
-  if (i % 2 !== 0) console.log(i);
-}
+for (let i = 0; i < 5; i++)
+  if (i % 2 !== 0) console.log(i)
