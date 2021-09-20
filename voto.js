4d7d90b82b8d4d9d98748d070f573986ceaee81f
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-20 01:05:09 -0300

    cleaning code

--- voto.js
+++ voto.js
@@ -2,14 +2,10 @@
 
 let idade = 15
 
-if (idade < 16) {
+if (idade < 16)
   console.log('Não vota')
-
-} else if (idade < 18 || idade > 65) {
+else if (idade < 18 || idade > 65)
   console.log('Voto facultativo')
-
-} else {
+else
   console.log('Voto obrigatório')
-
-}
 
