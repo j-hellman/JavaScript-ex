8d59cdfc65d7e3775c8f460b1dace4a093b76690
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-20 21:34:54 -0300

    botao zerar

--- contagem-intervalo/contagem.js
+++ contagem-intervalo/contagem.js
@@ -1,24 +1,35 @@
 
+let inicio = document.getElementById('txtinicio')
+let fim = document.getElementById('txtfim')
+let passo = document.getElementById('txtpasso')
+let btn = document.querySelector('button')
+let res = document.getElementById('res')
+let total = []
+
+//Emojis
+let emojis = [
+  128073, //mao 
+  127937 //bandeira
+]
+//
+
+// Botao contar
 function contar() {
-  let inicio = document.getElementById('txtinicio')
-  let fim = document.getElementById('txtfim')
-  let passo = document.getElementById('txtpasso')
-  let res = document.getElementById('res')
-  let total = []
-
-  //Emojis
-  let emojis = [
-    128073, //mao 
-    127937 //bandeira
-  ]
-  //
-
   for(let soma = Number(inicio.value); soma <= fim.value; soma += Number(passo.value)) {
     total.push(soma)
     total.push(String.fromCodePoint(emojis[0])) //Emoji mao
   }
   
   total.push(String.fromCodePoint(emojis[1])) //Emoji bandeira
-  res.innerHTML = 'Contando: <br />'
-  res.innerHTML += total
+  res.innerHTML = '<p>Contando: </p>'
+  res.innerHTML += total.join('') //Mostra o resultado do array sem as vírgulas
+}
+
+
+// Botao zerar
+function zerar() {
+  inicio.value = ''
+  fim.value = ''
+  passo.value = ''
+  res.innerHTML = '<p>Preparando a contagem...</p>'
 }
