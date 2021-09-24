fee9aadf39c5fc1f1bdab7c8475c6d2b4bb1e19f
Jaime Hellman <jaime_hellman@hotmail.com>
2021-09-23 23:23:43 -0300

    code clean

--- analisador-numeros/analisador.js
+++ analisador-numeros/analisador.js
@@ -1,76 +1,61 @@
-let list = []
-let cad = 0 //Total numeros cadastrados
-let maior = 0 //Var para maior valor
-let menor = 100 //Var para menor valor
-let soma = 0 //Var para soma valor
-let media = 0 //Var para media valor
-let res = document.getElementById('res') //Var para mensagens
-let tab = document.getElementById('analis') //Var para Lista Select 
-
+let valores = []
+let res = document.getElementById('res') //Mensagens
+let lista = document.getElementById('flista') //Lista Select 
 
 function adicionar() {
-  let num = document.getElementById('txtn') //Input numero
+  let num = document.getElementById('fnum') //Input numero
   let n = Number(num.value)
-
   
-  if (num.value.length == 0 || n < 1 || n > 100 || list.indexOf(n) !== -1) { //Validador do input número
+  if (n < 1 || n > 100 || valores.indexOf(n) != -1) { //Validador do input número
     alert('Valor inválido ou já encontrado na lista.')
-    num.value = '' //Limpa input numero
     
   } else {
-    res.innerHTML = '' //Limpa mensagens
-    list.push(n)
-    cad++
+    res.innerHTML = ''
+    valores.push(n)
     
     //Lista Select
     let item = document.createElement('option') 
     item.text = `Valor ${n} adicionado.`
-    tab.appendChild(item)
+    lista.appendChild(item)
     //
-    
-    //Calculos
-    if (n > maior) maior = n
-    if (n < menor) menor = n
-    soma += n
-    media = soma / list.length
-    //
-
-    num.value = '' //Limpa input numero
   }
+  num.value = ''
+  num.focus() //Cursor do mouse
 }
 
 
 function finalizar() {
-  if (list.length == 0) { //Validador campo Select
+  if (valores.length == 0) { //Validador campo Select
     alert('Adicione valores antes de finalizar.')
   
   } else {
-    res.innerHTML = '' //Limpa mensagens
+    res.innerHTML = ''
 
-    //Msg total numeros
-    let tot = document.createElement('p')
-    if (cad == 1) tot.innerHTML = `Ao todo, temos ${cad} número cadastrado`
-      else tot.innerHTML = `Ao todo, temos ${cad} números cadastrados`
-    res.appendChild(tot)
+    //Total numeros
+    let tot = valores.length
+    if (tot == 1) 
+      res.innerHTML += `<p>Ao todo temos ${tot} número cadastrado.</p>`
+    else 
+      res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+    //
 
-    //Msg maior valor
-    let mai = document.createElement('p')
-    mai.innerHTML = `O maior valor informado foi ${maior}.`
-    res.appendChild(mai)
-
-    //Msg menor valor
-    let men = document.createElement('p')
-    men.innerHTML = `O menor valor informado foi ${menor}.`
-    res.appendChild(men)
-
-    //Msg soma valor
-    let som = document.createElement('p')
-    som.innerHTML = `Somando todos os valores temos ${soma}.`
-    res.appendChild(som)
-
-    //Msg media valor
-    let med = document.createElement('p')
-    med.innerHTML = `A média dos valores digitados é ${media}.`
-    res.appendChild(med)
+    //Maior, menor, soma e media dos valores
+    let soma = 0
+    let media = 0
+    let maior = valores[0]
+    let menor = valores[0]
+    for (let pos in valores) {
+      soma += valores[pos]
+      media = soma / tot
+      if (valores[pos] > maior)
+        maior = valores[pos]
+      if (valores[pos] < menor)
+        menor = valores[pos]
+    }
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+    res.innerHTML += `<p>Somando todos os valores temo ${soma}.</p>`
+    res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    //
   }
 }
